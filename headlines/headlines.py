abd2bd6e2be4fde8a93fa25855598826ee479edb
J.R. Leeman <kd5wxb@gmail.com>
2017-05-03 08:00:27 -0600

    Add currency exchange rates with dropdowns

--- headlines/headlines.py
+++ headlines/headlines.py
@@ -15,7 +15,9 @@ rss_feeds = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
              'onion':'http://www.theonion.com/feeds/rss'}
 
 defaults = {'publication':'bbc',
-            'city':'London,UK'}
+            'city':'London,UK',
+            'currency_from':'GBP',
+            'currency_to':'USD'}
 
 @app.route('/')
 def home():
@@ -28,9 +30,22 @@ def home():
     if not city:
         city = defaults['city']
     weather = get_weather(city)
+
+    currency_from = request.args.get('currency_from')
+    if not currency_from:
+        currency_from = defaults['currency_from']
+    currency_to = request.args.get('currency_to')
+    if not currency_to:
+        currency_to = defaults['currency_to']
+    rate = get_rates(currency_from, currency_to)
+
     return render_template('home.html',
                            articles=articles,
-                           weather=weather)
+                           weather=weather,
+                           currency_from = currency_from,
+                           currency_to = currency_to,
+                           rate = rate)
+
 
 def get_news(query):
     if not query or query.lower() not in rss_feeds:
@@ -39,6 +54,15 @@ def get_news(query):
         publication = query.lower()
     feed = feedparser.parse(rss_feeds[publication])
     return feed['entries']
+
+
+def get_rates(frm, to):
+    currency_url = 'https://openexchangerates.org//api/latest.json?app_id={0}'.format(os.environ.get('OPENEXCHANGERATES_API_KEY', None))
+    all_currency = urlopen(currency_url).read()
+    parsed = json.loads(all_currency).get('rates')
+    frm_rate = parsed.get(frm.upper())
+    to_rate = parsed.get(to.upper())
+    return to_rate/frm_rate
 
 
 def get_weather(query):
@@ -51,7 +75,10 @@ def get_weather(query):
     if parsed.get('weather'):
         weather = {'description':parsed['weather'][0]['description'],
                    'temperature':parsed['main']['temp'],
-                   'city':parsed['name']}
+                   'city':parsed['name'],
+                   'country':parsed['sys']['country'],
+                   'windspeed':parsed['wind']['speed'],
+                   'winddeg':parsed['wind']['deg']}
     return weather
 
 
