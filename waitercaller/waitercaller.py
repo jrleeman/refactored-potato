8a5e8207973d048a0015f49736894b583a7a8e11
J.R. Leeman <kd5wxb@gmail.com>
2017-05-04 12:18:52 -0600

    Use bootstrap and insecure login

--- waitercaller/waitercaller.py
+++ waitercaller/waitercaller.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from flask.ext.login import LoginManager, login_required, login_user
+
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+
+DB = DBHelper()
 
 app = Flask(__name__)
+app.secret_key = 'NTOBiFxcjaehKa9nvgTmv5dslPUay7l4QDauEGIV3pSwpZKhpFGqJzestVyGODNT7BL8mauL38xyzgukYV3cIMix9eO8Jgb3bhvo'
+
+login_manager = LoginManager(app)
 
 @app.route('/')
 def home():
-    return "Under Construction"
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user_password = DB.get_user(email)
+    if user_password and user_password == password:
+        user = User(email)
+        login_user(user)
+        return redirect(url_for('account'))
+    return home()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
+
+
+@app.route('/account')
+@login_required
+def account():
+    return "You are logged in"
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
